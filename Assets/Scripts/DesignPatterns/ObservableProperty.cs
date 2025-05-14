627fc682169e572fb9d0d370c092c88429c0f3d1
Clean18 <cleanage95@gmail.com>
2025-05-14 11:08:49 +0900

    [Feat] 싱글톤, 옵저버 패턴 추가

---
+++ Assets/Scripts/DesignPatterns/ObservableProperty.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ObservableProperty<T>
+{
+	[SerializeField] private T _value;
+	public T Value
+	{
+		get { return _value; }
+		set
+		{
+			if (_value.Equals(value))
+				return;
+
+			// set에서 값이 변경되면 알림 전송
+			_value = value;
+			Notify();
+		}
+	}
+
+	private UnityEvent<T> onValueChanged = new();
+
+	public ObservableProperty(T value = default)
+	{
+		_value = value;
+	}
+
+	public void Subscribe(UnityAction<T> action)
+	{
+		onValueChanged.AddListener(action);
+	}
+
+	public void Unsubscribe(UnityAction<T> action)
+	{
+		onValueChanged.RemoveListener(action);
+	}
+
+	public void UnsubscribeAll()
+	{
+		onValueChanged.RemoveAllListeners();
+	}
+
+	private void Notify()
+	{
+		onValueChanged?.Invoke(Value);
+	}
+}
